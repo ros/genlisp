2a7376598d25dc7bd0f8a254fc6ee16bcca43b2d
Dirk Thomas <dthomas@willowgarage.com>
2012-09-21 12:58:35 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+
 from distutils.core import setup
-from catkin.package import parse_package_for_distutils
+from catkin_pkg.package import parse_package_for_distutils
 
 d = parse_package_for_distutils()
 d['packages'] = ['genlisp']
 d['package_dir'] = {'': 'src'}
 d['scripts'] = ['scripts/gen_lisp.py']
 d['install_requires'] = ['genmsg']
+
 setup(**d)
